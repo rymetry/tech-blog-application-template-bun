5fbdc940811e99659dfb6550468139ad03d513fd
rymetry <ryoma.saito2@gmail.com>
2025-09-28 20:16:10 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,12 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.microcms-assets.io',
         pathname: '/**',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: '**.microcms-assets.io',
+        pathname: '/**',
+      },
     ],
   },
 };
